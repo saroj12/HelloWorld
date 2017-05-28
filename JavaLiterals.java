9297953f48a56dc5e5f520062590320bfed1c4ee
Saroj Sharma <sarojkanta.sharma@gmail.com>
2017-05-28 08:14:06 +0530

    added new literal demo for short datratypes
    This sample program is to test the short execution for a 64 bit machine as short is 2 bytes 
    Currently the usage of short data type is very less as this was used for 16 bit processor in the earlier days.

---
+++ JavaLiterals.java
@@ -0,0 +1,21 @@
+package com.test.java;
+/************
+ * @author Saroj
+ * @version 1.0
+ * @since 05/28/2017
+ *
+ */
+public class JavaLiterals {
+
+	public static void main(String[] args) {
+		/**
+		 * Demo showing java literals and datatypes
+		 ****/
+		
+		
+		short _numberValue=12;
+		System.out.println("The short value is:::"+_numberValue);
+
+	}
+
+}
